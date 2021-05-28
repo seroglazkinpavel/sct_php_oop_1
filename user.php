454c10849fed1a5413b19bad779402dd8214537d
MSI <seroglazkinpavel@yandex.ru>
2021-05-28 23:20:37 +0400

    зменен файл user.php

--- user.php
+++ user.php
@@ -7,14 +7,64 @@ class User
     public $firstname;
     public $age;
 
+    public function __construct($lastname, $email, $firstname, $age){
+       $this->lastname = $lastname;
+       $this->email = $email;
+       $this->firstname = $firstname;
+       $this->age = $age;
+    }
+
+    public function getLastname(){
+        return $this->lastname;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getFirstname(){
+        return $this->firstname;
+    }
+
+    public function getAge(){
+        return $this->age;
+    }
+
+    public function setLastname($Lastname){
+        $this->lastname = $Lastname;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function setFirstname($firstname){
+        $this->firstname = $firstname;
+    }
+
+    public function setAge($age){
+        $this->age = $age;
+    }
 }
 
-$user = new User();
-$user->lastname = 'Иванов' . '<br />';
-$user->email = 'abc1@mail.ru' . '<br />';
-$user->firstname = 'Виктор' . '<br />';
-$user->age = 32 . '<br />';
+$user = new User('Иванов', 'abc1@mail.ru', 'Виктор', 32);
+
 echo $user->lastname . '<br />';
 echo $user->email . '<br />';
 echo $user->firstname . '<br />';
-echo $user->age;
+echo $user->age . '<br />';
+
+echo $user->getLastname() . '<br />';
+echo $user->getEmail() . '<br />';
+echo $user->getFirstname() . '<br />';
+echo $user->getAge();
+
+$user->setLastname('Иванов') . '<br />';
+$user->setEmail('qwer@mail.ru') . '<br />';
+$user->setFirstname('Виталий') . '<br />';
+$user->setAge(58);
+
+echo $user->getLastname() . '<br />';
+echo $user->getEmail() . '<br />';
+echo $user->getFirstname() . '<br />';
+echo $user->getAge();
