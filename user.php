3a1bfb0307842ff3deb0e84c881e314a4881f026
MSI <seroglazkinpavel@yandex.ru>
2021-05-29 16:15:53 +0400

    изменен файл user.php

--- user.php
+++ user.php
@@ -2,16 +2,16 @@
 
 class User
 {
-    public $lastname;
-    public $email;
-    public $firstname;
-    public $age;
+    private $lastname;
+    private $email;
+    private $firstname;
+    private $age;
 
     public function __construct($lastname, $email, $firstname, $age){
-       $this->lastname = $lastname;
-       $this->email = $email;
-       $this->firstname = $firstname;
-       $this->age = $age;
+        if (!empty($lastname)) $this->lastname = $lastname;
+        if (!empty($email)) $this->email = $email;
+        if (!empty($firstname)) $this->firstname = $firstname;
+        if (!empty($age)) $this->age = $age;
     }
 
     public function getLastname(){
@@ -49,20 +49,15 @@ class User
 
 $user = new User('Иванов', 'abc1@mail.ru', 'Виктор', 32);
 
-echo $user->lastname . '<br />';
-echo $user->email . '<br />';
-echo $user->firstname . '<br />';
-echo $user->age . '<br />';
-
 echo $user->getLastname() . '<br />';
 echo $user->getEmail() . '<br />';
 echo $user->getFirstname() . '<br />';
 echo $user->getAge();
 
-$user->setLastname('Иванов') . '<br />';
+$user->setLastname('Сидоров') . '<br />';
 $user->setEmail('qwer@mail.ru') . '<br />';
 $user->setFirstname('Виталий') . '<br />';
-$user->setAge(58);
+$user->setAge(58) . '<br />';
 
 echo $user->getLastname() . '<br />';
 echo $user->getEmail() . '<br />';
