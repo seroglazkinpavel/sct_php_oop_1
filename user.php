9c8c56d3dc41f66dfd976681be9a255f3b00c335
MSI <seroglazkinpavel@yandex.ru>
2021-06-05 20:25:32 +0400

    изменен файл user.php

--- user.php
+++ user.php
@@ -6,60 +6,64 @@ class User
     private $email;
     private $firstname;
     private $age;
+    public static $counter = 0;
 
     public function __construct($lastname, $email, $firstname, $age){
         if (!empty($lastname)) $this->lastname = $lastname;
         if (!empty($email)) $this->email = $email;
         if (!empty($firstname)) $this->firstname = $firstname;
         if (!empty($age)) $this->age = $age;
+        self::$counter++;
     }
 
-    public function getLastname(){
+    public static function getCounter()
+    {
+        return self::$counter;
+    }
+
+    public function getLastname()
+    {
         return $this->lastname;
     }
 
-    public function getEmail(){
-        return $this->email;
-    }
-
-    public function getFirstname(){
-        return $this->firstname;
-    }
-
-    public function getAge(){
-        return $this->age;
-    }
-
-    public function setLastname($Lastname){
+    public function setLastname($Lastname)
+    {
         $this->lastname = $Lastname;
     }
 
-    public function setEmail($email){
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    public function setFirstname($firstname){
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname($firstname)
+    {
         $this->firstname = $firstname;
     }
 
-    public function setAge($age){
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
         $this->age = $age;
     }
 }
 
 $user = new User('Иванов', 'abc1@mail.ru', 'Виктор', 32);
+$user1 = new User('Иванов', 'abc1@mail.ru', 'Виктор', 32);
+$user2 = new User('Иванов', 'abc1@mail.ru', 'Виктор', 32);
 
-echo $user->getLastname() . '<br />';
-echo $user->getEmail() . '<br />';
-echo $user->getFirstname() . '<br />';
-echo $user->getAge();
-
-$user->setLastname('Сидоров') . '<br />';
-$user->setEmail('qwer@mail.ru') . '<br />';
-$user->setFirstname('Виталий') . '<br />';
-$user->setAge(58) . '<br />';
-
-echo $user->getLastname() . '<br />';
-echo $user->getEmail() . '<br />';
-echo $user->getFirstname() . '<br />';
-echo $user->getAge();
+echo $user::getCounter();
