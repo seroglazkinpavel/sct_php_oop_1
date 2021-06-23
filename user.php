fd8211562a4a926faa5604044f5edaa7331997fe
MSI <seroglazkinpavel@yandex.ru>
2021-06-23 13:18:10 +0400

    изменен файл user.php

--- user.php
+++ user.php
@@ -3,28 +3,29 @@
 abstract class Vehicle
 {
 
-    abstract public function getName($weight, $year_of_issue);
+    abstract public function getName();
 
 }
 
 class Bus extends Vehicle
 {
-
     public $year_of_issue;
     public $weight;
 
-    public function getName($weight, $year_of_issue)
+    public function __construct($weight, $year_of_issue)
     {
         $this->weight = $weight;
-        $this->year_of_issue = $year_of_issue;
-        echo 'Автобус весом '.$weight. ' кг и '.$year_of_issue.' года выпуска';
+        $this->year_of_issue = $year_of_issue; 
     }
 
+    public function getName()
+    {
+        echo 'Автобус весом '.$this->weight. ' кг и '.$this->year_of_issue.' года выпуска';
+    }
 }
 
 $bus = new Bus();
 /*Если не реализовать абстрактный метод в дочерних классах то возникает ошибка (Fatal error: Class Bus contains 1 abstract
 method and must therefore be declared abstract or implement the remaining methods (Vehicle::getName)
 in C:\OpenServer\domains\oop\user.php on line 16)*/
-$bus = new Bus();
-$bus->getName(700, 2002);
+$bus->getName();
