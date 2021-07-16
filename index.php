efa0465fa3c5175570bd5101518b0f8874a33da9
MSI <seroglazkinpavel@yandex.ru>
2021-07-16 22:27:01 +0400

    удалены папки, изменен файл

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
+// Если мы в файле .htaccess в php_value error_reporting 0; установим 0, ошибки не будут  показываться
+	error_reporting(0);
+	$array = [1, 2, 3,];
+	print_r($array)  
 
-use lib\Car;
-use lib\Aircraft;
+// Если мы в файле .htaccess в php_value error_reporting -1; установим -1, ошибки будут  показываться
+	error_reporting(E_ALL);
+	$array = [1, 2, 3,];
+	print_r($array)  // Parse error: syntax error, unexpected end of file in C:\OpenServer\domains\oop\index.php on line 23
 
-//require_once 'Car.php';
-//require_once 'Aircraft.php';
-set_include_path(get_include_path() . PATH_SEPARATOR . 'lib');
-spl_autoload_extensions('.php');
-spl_autoload_register();
-
-$car = new Car();
-$car->move();
-echo '<br>';
-$aircraft = new Aircraft();
-$aircraft->fly();
+	// error_reporting — Задаёт, какие ошибки PHP попадут в отчёт
+	// error_reporting(E_ERROR | E_WARNING | E_PARSE)-так можно указать несколько констант в функции error_reporting;
