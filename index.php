c5b5ecf6e233c20fe66a49f855fc73bec5e82932
MSI <seroglazkinpavel@yandex.ru>
2021-07-22 18:48:59 +0400

    изменен файл

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
-// Если мы в файле .htaccess в php_value error_reporting 0; установим 0, ошибки не будут  показываться
-	error_reporting(0);
-	$array = [1, 2, 3,];
-	print_r($array)  
 
-// Если мы в файле .htaccess в php_value error_reporting -1; установим -1, ошибки будут  показываться
-	error_reporting(E_ALL);
-	$array = [1, 2, 3,];
-	print_r($array)  // Parse error: syntax error, unexpected end of file in C:\OpenServer\domains\oop\index.php on line 23
+class  Valid
+{
 
-	// error_reporting — Задаёт, какие ошибки PHP попадут в отчёт
-	// error_reporting(E_ERROR | E_WARNING | E_PARSE)-так можно указать несколько констант в функции error_reporting;
+    public static function validEmail($email)
+    {
+
+        if ($email == '') {
+            throw new Exception('E-mail не указан');
+        } elseif (mb_strlen($email) > 255) {
+            throw new Exception('Слишком длинный e-mail');
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception('E-mail указан неправильно');
+        } else return $email;
+    }
+}
+
+try {
+    echo Valid::validEmail('eeeeeee');
+} catch (Exception $e) {
+    echo '<br />Возникла ошибка: ' . $e->getMessage();
+}
