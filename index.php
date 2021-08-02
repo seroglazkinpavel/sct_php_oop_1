46fdd2471fa04d23b81eae0f38f444226a9571b1
MSI <seroglazkinpavel@yandex.ru>
2021-08-02 23:28:58 +0400

    изменен файл

--- index.php
+++ index.php
@@ -1,38 +1,40 @@
 <?php
-
-class  EmptyException extends Exception
-{
-
+try {
+    $x = 5 % 0;
+    //intdiv(5, 0);
+    //$str = 'Hello';
+    //$str[] = 2;
+    //intdiv(PHP_INT_MIN, -1);
+    //eval("5 + '");
+    //AssertionError assert()
+} catch (Error $e) {
+    echo $e->getMessage() . '<br>';
+    echo $e->getCode() . '<br>';
+    echo $e->getFile() . '<br>';
+    echo $e->getLine();
 }
 
-class  LongException extends Exception
-{
+/*
+Error - базовый класс для всех внутренних ошибок PHP.
+Error
+Добавлено в PHP7 для обработки фатальных ошибок. То есть многие из ошибок, которые раньше приводили к Fatal Error,
+в PHP7 могут обрабатываться в блоках try/catch. Эти ошибки вызываются самим PHP, нет нужды их вызывать, как Exception.
+Класс Error имеет три подкласса:
 
-}
+AssertionError
+Вызывается, когда условие, заданное методом assert(), не выполняется.
 
-class   InvalidException extends Exception
-{
-
-}
-
-class  Valid
-{
-
-    public static function validEmail($email)
-    {
-
-        if ($email == '') {
-            throw new EmptyException('E-mail не указан');
-        } elseif (mb_strlen($email) > 255) {
-            throw new LongException('Слишком длинный e-mail');
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new InvalidException('E-mail указан неправильно');
-        } else return $email;
-    }
-}
+ParseError
+Для ошибок парсинга, когда подключаемый по include/require код вызывает ошибку синтаксиса, ошибок функции eval() и т.п.
 
 try {
-    echo Valid::validEmail('');
-} catch (Exception $e) {
-    echo '<br />Возникла ошибка: ' . $e->getMessage();
+   require 'file-with-syntax-error.php';
+} catch (ParseError $e) {
+   // обработка ошибки
 }
+TypeError
+Используется для ошибок несоответствия типов данных. В PHP7 введена опциональная строгая типизация. Вот для поддержки
+ошибок, связанных с ней, и служит этот класс. Например, если функция ожидает на входе аргумент типа int, а вы ее
+вызываете со строковым аргументом.
+
+if (!is_string($name)) throw new TypeError('Имя должно быть стройкой!');*/
